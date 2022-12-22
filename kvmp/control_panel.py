c6184944802a0d4af58c064316553bd960754e37
Aleksander Tacconi <aleks.tacconi@gmail.com>
2022-12-22 14:28:59 +0000

    added csrf

--- kvmp/control_panel.py
+++ kvmp/control_panel.py
@@ -3,16 +3,22 @@ from flask_assets import Bundle, Environment
 from kvmp.instance import instances
 import libvirt
 import sys
+from flask_seasurf import SeaSurf
 
 # https://testdriven.io/blog/flask-htmx-tailwind/
 
 app = Flask(__name__)
+app.secret_key = 'gfdsgsfdgfdgdsfgdfgfdgdfgsgfdgdfg55efdgfsdt5estdt'
 assets = Environment(app)
 css = Bundle("src/main.css", output="dist/main.css")
 
 assets.register("css", css)
 css.build()
 
+csrf = SeaSurf(app)
+
+
+@csrf.exempt
 @app.route("/")
 def hello_world():
     conn = libvirt.open('qemu:///system')
@@ -23,12 +29,14 @@ def hello_world():
     return render_template('index.html', instances=instances)
 
 
+@csrf.exempt
 @app.route("/system")
 def system():
     conn = libvirt.open('qemu:///system')
     return conn.getSysinfo()
 
 
+@csrf.exempt
 @app.route("/search", methods=["POST"])
 def search_instance():
     search_term = request.form.get("search")
@@ -41,6 +49,7 @@ def search_instance():
     return render_template("instance.html", instances=res_instances)
 
 
+@csrf.exempt
 @app.route("/render_instances")
 def render_instances():
     return render_template("instance.html", instances=instances)
